6faa6eab06b1dd8e8afd0edee908c6ae639e6be7
elena tarasova <nafanichka.ev@gmail.com>
2023-02-01 10:43:53 -0500

    HW2

---
+++ scripts.js
@@ -0,0 +1,34 @@
+//<script language="JavaScript">
+    //alert( `result: ${158 + 2}` );
+
+    //let name = "Elena";
+    //alert('Hello, ' + name);
+
+    //let n = prompt("Input your name");
+    //alert('Hello, ' + n);
+
+    //function hello(i){
+    //    alert(`Hello, ${i}!`)
+    //}
+    //let i = prompt('What is your name', 'Name')
+    //hello(i)
+    //let r = confirm ('Are you sure?')
+    //if (r == true) {
+    //    alert ('We are happy that you sure')}
+    //else{
+    //    alert ('Sorry that you nor sure')
+    //}
+   let age = parseInt(prompt("Input your age", "Age"));
+    switch (age) {
+        case 18: alert('Now you are adult, you can do anything!');
+        break;
+        case 10: alert('Go to study little boy!');
+        break;
+        case 30: alert('Go sleep, tomorrow you have to go to office');
+        break;
+        default: alert('We don't know what you have to do');
+        break;
+
+
+
+</script>
